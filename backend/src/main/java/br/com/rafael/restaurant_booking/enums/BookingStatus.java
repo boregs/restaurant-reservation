0c7a0fc06be7374724d4955b7448c8d8d0d84901
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-09-24 14:54:47 -0300

    feat(backend): enumerators for status and table location

---
+++ backend/src/main/java/br/com/rafael/restaurant_booking/enums/BookingStatus.java
@@ -0,0 +1,7 @@
+package br.com.rafael.restaurant_booking.enums;
+
+public enum BookingStatus {
+    ACTIVE,
+    CANCELLED,
+    RESCHEDULED
+}
---
+++ backend/src/main/java/br/com/rafael/restaurant_booking/enums/TableStatus.java
@@ -0,0 +1,6 @@
+package br.com.rafael.restaurant_booking.enums;
+
+public enum TableStatus {
+    AVAILABLE,
+    NOT_AVAILABLE
+}
